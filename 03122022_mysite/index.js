fbd6161ed9f7a062cfeb8298859479a2eddb9c2f
Ochwada <Ochwada@users.noreply.github.com>
2022-12-08 13:27:19 +0100

    js

---
+++ 03122022_mysite/index.js
@@ -0,0 +1,18 @@
+/* alert('hello');
+alert('world');
+
+https://github.com/rwaldron/idiomatic.js/ */
+
+function test(){
+    var a = "3";
+    var b = "8";
+}
+    var c = b;
+    var a = b;
+    var b = c;
+    
+    
+
+
+    console.log('a is' + a);
+    console.log('b is' + b);
