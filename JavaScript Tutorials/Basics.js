8b1bc979cf49548224c9394b4667f62f511c38f3
Ochwada <Ochwada@users.noreply.github.com>
2022-12-09 14:19:28 +0100

    File rename

--- JavaScript Tutorials/Basics.js
+++ JavaScript Tutorials/Basics.js
@@ -62,3 +62,27 @@ function getMilk(cost, charge){
 
 // ---------------New code line----------------------------
 // ---------------------  ----------
+
+//Create your function below this line.
+//The first parameter should be the weight and the second should be the height.
+
+
+function bmiCalculator(weight, height) {
+    // var bmi = weight / (height ** 2);
+    var bmi = weight / Math.pow(height,2);
+
+    return bmi;
+    // return Math.floor(bmi);
+    // return Math.round(bmi);
+}
+
+console.log(bmiCalculator(65, 1.8));
+
+
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+
+var bmi = bmiCalculator(65, 1.8); 
+
+bmi should equal 20 when it's rounded to the nearest whole number.
+
+*/
