54c4f4c2f8d8ecf792aa3ee7b07e8d6e964d2b11
Ochwada <Ochwada@users.noreply.github.com>
2022-12-12 18:01:57 +0100

    Guest list check

--- JavaScript Tutorials/IntermediateJS.js
+++ JavaScript Tutorials/IntermediateJS.js
@@ -138,14 +138,21 @@ guestlist_check()
 var output = [];
 var count = 1;
 
-function fizzBuzz() {    
-    if (count % 3 === 0) {
+function fizzBuzz() {  
+    if (count % 3 === 0 && count % 5 === 0) {
+        output.push("fizzBuzz")
+    }else if (count % 3 === 0) {
         output.push("Fizz");
-    } else {
-        output.push(count);
+    } else if(count % 5 === 0){
+        output.push("Buzz");
+    } else{
+        output.push(count)
     }
 
 count++;
 
     console.log(output);
 }
+
+// ---------------------Array  ---------
+// ---------------------Who is buying Lunch--
