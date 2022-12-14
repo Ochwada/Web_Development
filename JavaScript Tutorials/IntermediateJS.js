e822fce6c80fa5b9656c95bac327cbf75df92c5e
Ochwada <Ochwada@users.noreply.github.com>
2022-12-14 20:12:03 +0100

    fizzBuzz challange

--- JavaScript Tutorials/IntermediateJS.js
+++ JavaScript Tutorials/IntermediateJS.js
@@ -135,24 +135,45 @@ guestlist_check()
 // ---------------------Array  ---------
 // ---------------------buzz-Fizzbuzz--
 // Buzz quiz
+
 var output = [];
 var count = 1;
 
-function fizzBuzz() {  
-    if (count % 3 === 0 && count % 5 === 0) {
-        output.push("fizzBuzz")
-    }else if (count % 3 === 0) {
-        output.push("Fizz");
-    } else if(count % 5 === 0){
-        output.push("Buzz");
-    } else{
-        output.push(count)
-    }
-
-count++;
-
-    console.log(output);
+function fizzbuzz() {
+  while (count <=100) {
+      
+  
+        if (count % 3  === 0 && count % 5 == 0) {
+            output.push("FizzBuzz");
+        } else if (count % 3 === 0) {
+            output.push("Fizz");
+        }else if (count % 5 === 0) {
+            output.push("Buzz");
+        }else{
+            output.push(count);
+        }
+   
+        count++;
+       }
+        console.log(output);
 }
+
+console.log(fizzbuzz());
+
+
 
 // ---------------------Array  ---------
 // ---------------------Who is buying Lunch--
+
+var names =  ["Angela", "Ben", "Jenny", "Michael", "Chloe"]
+
+function whosPaying() {
+   
+     return names[Math.floor(Math.random() * names.length)] + " is going to buy lunch today !";
+    
+}
+
+console.log(whosPaying());
+
+// ---------------------Loops  ---------
+// ---------------------Control Statements: While Loop--
