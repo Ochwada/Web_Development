c1bd0be90206b8e958412a3f3857593fa0ed7fe9
Ochwada <Ochwada@users.noreply.github.com>
2022-12-14 20:20:40 +0100

    fizzBuzz challange 2

--- JavaScript Tutorials/IntermediateJS.js
+++ JavaScript Tutorials/IntermediateJS.js
@@ -162,6 +162,23 @@ console.log(fizzbuzz());
 
 
 
+// or
+
+function fizzbuzz( ) {
+    for (var i = 1; i <= 100; i++){
+         if (i % 3 === 0 && i % 5 === 0) {
+              console.log("fizzBuzz")
+         }else if (i % 3 === 0) {
+              console.log("fizz")
+         } else if(i % 5 === 0) {
+              console.log("buzz")
+         }else{
+              console.log(i)
+         }
+    }
+}
+
+console.log(fizzbuzz())
 // ---------------------Array  ---------
 // ---------------------Who is buying Lunch--
 
