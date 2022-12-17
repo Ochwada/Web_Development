66d74f75d12bcf7d9d8253c81058b12bae809c1c
Ochwada <Ochwada@users.noreply.github.com>
2022-12-17 12:06:24 +0100

    Dicee project starting

--- DOM_DocumentObjectModel/main.js
+++ DOM_DocumentObjectModel/main.js
@@ -2,3 +2,12 @@
 
 /*  ---- Document Object Model ----->
  ---- --------------------- --------> */
+/* document.getElementsByClassName("btn")[0].style.color = "red";
+document.querySelector("#list .item");
+​document.querySelectorAll("#list .item");
+document.querySelector("#list .item a").innerHTML = "Second Google Link";
+document.querySelector("#list .item a").style.color = "pink"
+document.querySelector("h1").classList.toggle("huge");
+document.querySelector("h1").innerHTML = "<em>Good Bye </em>"
+document.querySelector("a").setAttribute("href", "https://www.bing.com");
+*/
