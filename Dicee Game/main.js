7421019ca2688dd2595cb53ae06de6046a8be9e0
Ochwada <nakhulolinda@yahoo.com>
2022-12-20 22:54:48 +0100

    dicee cont ..

--- Dicee Game/main.js
+++ Dicee Game/main.js
@@ -1,9 +1,9 @@
-let diceface = new Array("images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png")
 
-function chooseDice(){
+let path = new Array("images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png")
+
+function chooseDice(imgAr, path){
     let randomNumber1 = Math.floor(Math.random() * diceface.length);
-    // document.querySelector(".squire").setAttribute("images/dice6.png", randomNumber1);
-    document.getElementById(".squire").innerHTML = randomNumber1;
+    return path[randomNumber1];
 
 }
 
@@ -12,6 +12,7 @@ function myFunction() {
     document.getElementById("btn").innerHTML = "Hello World";
   }
 
+  //how to create a random equation to pick a picture from list?
   /*  ---- Document Object Model ----->
  ---- --------------------- --------> */
 /* document.getElementsByClassName("btn")[0].style.color = "red";
@@ -23,3 +24,5 @@ document.querySelector("h1").classList.toggle("huge");
 document.querySelector("h1").innerHTML = "<em>Good Bye </em>"
 document.querySelector("a").setAttribute("href", "https://www.bing.com");
 */
+
+//connect to mangodb in node.js?
