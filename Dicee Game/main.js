0a6bab4f55d7d232bc4413f3d5ab239957b0b52c
Ochwada <nakhulolinda@yahoo.com>
2022-12-21 20:53:29 +0100

    dicee cont ..

--- Dicee Game/main.js
+++ Dicee Game/main.js
@@ -1,8 +1,8 @@
-
+/* 
 let path = new Array("images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png")
 
 function chooseDice(imgAr, path){
-    let randomNumber1 = Math.floor(Math.random() * diceface.length);
+    let randomNumber1 = Math.floor(Math.random() * path.length);
     return path[randomNumber1];
 
 }
@@ -11,18 +11,11 @@ function chooseDice(imgAr, path){
 function myFunction() {
     document.getElementById("btn").innerHTML = "Hello World";
   }
+ */
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-  //how to create a random equation to pick a picture from list?
-  /*  ---- Document Object Model ----->
- ---- --------------------- --------> */
-/* document.getElementsByClassName("btn")[0].style.color = "red";
-document.querySelector("#list .item");
-​document.querySelectorAll("#list .item");
-document.querySelector("#list .item a").innerHTML = "Second Google Link";
-document.querySelector("#list .item a").style.color = "pink"
-document.querySelector("h1").classList.toggle("huge");
-document.querySelector("h1").innerHTML = "<em>Good Bye </em>"
-document.querySelector("a").setAttribute("href", "https://www.bing.com");
-*/
+let randomDiceImage = "images/dice" + randomNumber1 + ".png";
 
-//connect to mangodb in node.js?
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", "randomDiceImage");
