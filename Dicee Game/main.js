63be0f194e02edcc959b43952aba6224e907fe19
Ochwada <Ochwada@users.noreply.github.com>
2022-12-22 16:14:48 +0100

    Dice shuffle works

--- Dicee Game/main.js
+++ Dicee Game/main.js
@@ -1,21 +1,29 @@
-/* 
-let path = new Array("images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png")
+// Dice 1
 
-function chooseDice(imgAr, path){
-    let randomNumber1 = Math.floor(Math.random() * path.length);
-    return path[randomNumber1];
 
-}
-
-// document.getElementById("button").onclick = chooseDice()
-function myFunction() {
-    document.getElementById("btn").innerHTML = "Hello World";
-  }
- */
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-
 let randomDiceImage = "images/dice" + randomNumber1 + ".png";
+var image1 = document.querySelectorAll("#dice img")[0];
+image1.setAttribute("src", randomDiceImage);
 
-var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", "randomDiceImage");
+// Dice 2
+randomNumber2 = Math.floor(Math.random() *6) +1;
+randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("#dice img")[1];
+image2.setAttribute("src", randomDiceImage2);
+
+// IMAGE -- Flag
+
+/* flag = new Image(100, 150);
+flag.src = "images/flag.png";
+
+ */
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML =" 🚩 Player 1 Wins";
+}else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML =" 🚩 Player 2 Wins";
+ }else{
+    document.querySelector("h1").innerHTML = "Its a Draw"
+ }
