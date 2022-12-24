aed6aadcaf389950f285eb2082da53c348cec69d
Ochwada <Ochwada@users.noreply.github.com>
2022-12-24 13:35:13 +0100

    Drum kit... continue

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -14,12 +14,60 @@ document.querySelectorAll("button")[2].addEventListener("click", function () {
 }); 
  */
 
-var noOfDrumButtons= document.querySelectorAll(".drum").length;
+var noOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i=0; i< noOfDrumButtons ; i++) {
+for (var i = 0; i < noOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        var audio = new Audio("sounds/tom-1.mp3")
-        audio.play();
+
+        //this.style.color = "white";
+        //switchStatements
+        var buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+            case "w":
+                var crash = new Audio("sounds/crash.mp3")
+                crash.play()
+                break;
+
+            case "a":
+                var kick_bass = new Audio("sounds/kick-bass.mp3")
+                kick_bass.play()
+                break;
+
+            case "s":
+                var snare = new Audio("sounds/snare.mp3")
+                snare.play()
+                break;
+
+            case "d":
+                var tom_1 = new Audio("sounds/tom-1.mp3")
+                tom_1.play()
+                break;
+
+            case "j":
+                var tom_2 = new Audio("sounds/tom-2.mp3")
+                tom_2.play()
+                break;
+
+            case "k":
+                var tom_3 = new Audio("sounds/tom-3.mp3")
+                tom_3.play()
+                break;
+
+            case "l":
+                var tom_4 = new Audio("sounds/tom-4.mp3")
+                tom_4.play()
+                break;
+
+            default:
+                console.log(buttonInnerHTML)
+                break;
+        }
+
+
     });
 };
 
+/*
+var audio = new Audio("sounds/tom-1.mp3")
+audio.play(); */
+//switch statement? 
