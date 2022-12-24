35af646baa777e4345698433f75830dcd24b2f85
Ochwada <Ochwada@users.noreply.github.com>
2022-12-24 12:28:24 +0100

    Drum kit... continue

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,7 +1,25 @@
 /* alert("hello world") */
-
+/* 
 document.querySelector("button").addEventListener("click", handleClick);
 
-function handleClick(){
-    alert("I got clicked")
+function handleClick() {
+    alert("w got clicked")
 }
+document.querySelector(".w").addEventListener("click", function () {
+alert("w got clicked")
+});
+
+document.querySelectorAll("button")[2].addEventListener("click", function () {
+    alert("s got clicked")
+}); 
+ */
+
+var noOfDrumButtons= document.querySelectorAll(".drum").length;
+
+for (var i=0; i< noOfDrumButtons ; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var audio = new Audio("sounds/tom-1.mp3")
+        audio.play();
+    });
+};
+
