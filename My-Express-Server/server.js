dd04dcb50495842a9d3a634186abf7a8f630e099
Ochwada <Ochwada@users.noreply.github.com>
2022-12-25 18:01:24 +0100

    Express.js, Node.js and my 1st Server

--- My-Express-Server/server.js
+++ My-Express-Server/server.js
@@ -2,6 +2,23 @@ const express = require('express');
 const app = express();
 
 
+// ---------START  pages -------------
+app.get("/", function(request, response){
+    //console.log(request);
+    response.send("<h2> Hello World !</h2>")
+});
+
+app.get("/contact", function(request, response){
+    response.send("contact me at @ochwada")
+});
+
+
+app.get("/about", function(request, response){
+    response.send(" My Name is <strong> Linda Ochwada </strong>. <br/> I like traveling and am learning coding")
+})
+// ----------- end of pages -----------
+
 app.listen(3000, function() {
     console.log('Server started on port 3000')
 });
+
