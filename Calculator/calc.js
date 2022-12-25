a2f99aff1ea55f7ea6597094996df818b653963e
Ochwada <Ochwada@users.noreply.github.com>
2022-12-25 18:52:57 +0100

    My calculator- finished

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,13 +1,33 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
+
 const app = express();
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // ---------- START Pages ----------------->
 app.get('/', (req, res) => {
-    res.send("<h1> Hello World!</h1>")
+    res.sendFile(__dirname + '/index.html');
 });
+
+
+
+app.post('/', (req, res) => {
+
+    //console.log(req.body.num1);
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+    res.send("The result of the Calculation is: " + result);
+})
+
 // ----------End Pages ----------------->
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('Server started on port 3000');
 })
