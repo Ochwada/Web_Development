ae66f881f22efafa496c94babe3619fb3a491394
Ochwada <Ochwada@users.noreply.github.com>
2022-12-25 14:57:30 +0100

    Simon Game Challenge - finished game

--- Simon Game Challenge Starting Files/main.js
+++ Simon Game Challenge Starting Files/main.js
@@ -22,12 +22,12 @@ let level = 0;
 time, call nextSequence(). */
 $(document).keydown(function () {
 
-    if(!started) {
+    if (!started) {
         $("#level-title").text("Level" + level);
         nextSequence();
         started = true;
     }
-    
+
 });
 
 
@@ -120,20 +120,48 @@ function checkAnswer(currentLevel) {
     //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same 
     //as the game pattern. If so then log "success", otherwise log "wrong".
 
-    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
         console.log("success");
-        
-    //4. If the user got the most recent answer right in step 3, then check that they have finished 
-    //their sequence with another if statement.
 
-        if(userClickedPattern.length === gamePattern.length){
+        //4. If the user got the most recent answer right in step 3, then check that they have finished 
+        //their sequence with another if statement.
+
+        if (userClickedPattern.length === gamePattern.length) {
             //5. Call nextSequence() after a 1000 millisecond delay.
 
             setTimeout(function () {
                 nextSequence();
             }, 1000);
         }
-    }else{
-        console.log("wrong")
+    } else {
+        console.log("wrong");
+
+        //In the sounds folder, there is a sound called wrong.mp3, play this sound if the user 
+        //got one of the answers wrong.
+
+        playSound("wrong");
+        // In the styles.css file, there is a class called "game-over", apply this class to the body of the website when the
+        // user gets one of the answers wrong and then remove it after 200 milliseconds.
+
+        $("body").addClass("game-over");
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        // Change the h1 title to say "Game Over, Press Any Key to Restart" if the user got the answer wrong.
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        startOver();
     }
+};
+
+
+/* ----------------------New FUNCTION--------------------------- */
+/* ------------------------------------------------------------- */
+/* . Create a new function called startOver(). */
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
 };
