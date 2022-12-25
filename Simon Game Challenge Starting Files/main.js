297c6ceb7085648e3d1d7ee6666c6414bbd848fc
Ochwada <Ochwada@users.noreply.github.com>
2022-12-25 13:21:33 +0100

    Simon Game Challenge - Press A key to Start

--- Simon Game Challenge Starting Files/main.js
+++ Simon Game Challenge Starting Files/main.js
@@ -8,12 +8,30 @@ let buttonColors = ["red", "blue", "green", "yellow"]
 let gamePattern = [];
 let userClickedPattern = [];
 
+/* You'll need a way to keep track of whether if the game has started or not, so you only call 
+nextSequence() on the first keypress. */
+let started = false;
 
+//2. Create a new variable called level and start at level 0.
+let level = 0;
+/* ----------------------New FUNCTION--------------------------- */
+/* ------------------------------------------------------------- */
+/*  detect when a keyboard key has been pressed, when that happens for the first 
+time, call nextSequence(). */
+$(document).keydown(function () {
+
+    if(!started) {
+        $("#level-title").text("Level" + level);
+        nextSequence();
+        started = true;
+    }
+    
+});
 /* ----------------------New FUNCTION--------------------------- */
 /* ------------------------------------------------------------- */
 
 /*  detect when any of the buttons are clicked and trigger a handler function.*/
-$(".btn").on("click", function() {
+$(".btn").on("click", function () {
 
     /* In  the handler, create a new variable called userChosenColor to store the 
     id of the button that got clicked.*/
@@ -31,7 +49,7 @@ $(".btn").on("click", function() {
 
 /* ----------------------New FUNCTION--------------------------- */
 /* ------------------------------------------------------------- */
-function playSound(name){
+function playSound(name) {
     /* play the sound for the button color selected */
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
@@ -41,19 +59,33 @@ function playSound(name){
 /* ----------------------New FUNCTION--------------------------- */
 /* ------------------------------------------------------------- */
 
-function animetePress(currentColor){
+function animetePress(currentColor) {
     let activeButton = $("." + currentColor);
-    
+
     activeButton.addClass("pressed");
-    setTimeout(function(){
-        activeButton.removeClass("pressed"); 
+    setTimeout(function () {
+        activeButton.removeClass("pressed");
     }, 100);
+
+
+    /*    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+         $("#" + currentColor).removeClass("pressed");
+     }, 100) */
 };
 /* ----------------------New FUNCTION--------------------------- */
 /* ------------------------------------------------------------- */
 /* Generate a new random number between 0 and 3, and store it in a variable 
 called randomNumber */
 function nextSequence() {
+
+
+    //4. Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+    level++;
+
+    //5. Inside nextSequence(), update the h1 with this change in the value of level.
+    $("#level-title").text("Level " + level);
+
     let randomNumber = Math.floor(Math.random() * 4);
 
     /*Create a new variable called randomChosenColor and use the randomNumber 
@@ -67,7 +99,6 @@ function nextSequence() {
     Animate a flash to the button selected */
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    
-};
 
+};
 
