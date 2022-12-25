85a18ee5e09bc2cfd1c8ca659e7677bf2e6d27b4
Ochwada <Ochwada@users.noreply.github.com>
2022-12-25 16:58:34 +0100

    node intro

---
+++ Node/main.js
@@ -0,0 +1,9 @@
+
+var superheroes = require('superheroes');
+var supervillains = require("supervillains");
+
+var mySuperhero= superheroes.random();
+var mySupervillains = supervillains.random();
+
+console.log("My Superhero Name: "+ mySuperhero);
+console.log("My Villian Name: " +  mySupervillains);
