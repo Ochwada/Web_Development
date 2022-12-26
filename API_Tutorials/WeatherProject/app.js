4d568465233b70ad0bb97eb489c79e64006bf1a1
Ochwada <Ochwada@users.noreply.github.com>
2022-12-26 15:41:16 +0100

    creating the app

--- API_Tutorials/WeatherProject/app.js
+++ API_Tutorials/WeatherProject/app.js
@@ -1,53 +1,85 @@
 const express = require('express');
 const https = require('node:https'); // Native Node modules, no need to install
-
+const bodyParser = require('body-parser');
 
 
 const app = express();
+
+/* -- Have the app use the body-parser -- */
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 /* --------    Web pages     ---------------*/
 
 
 
 app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+    //res.send("Server is up and running")
+});
+/* --- catch the html form ----*/
 
-    //res.sendFile( __dirname + '/index.html');
+app.post('/', (req, res) => {
 
-    var urlLink = "https://api.openweathermap.org/data/2.5/weather?q=Spandau,%20Berlin&appid=fd88392e09391d414afd29129da77337&unit=metric"
+    const query = req.body.cityName
+    const apiKey = "fd88392e09391d414afd29129da77337"
+    const units = "metric"
+    const urlLink = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&unit=" + units;
 
     https.get(urlLink, (response) => {
         console.log(response.statusCode);
 
         response.on('data', (data) => {
-            // convert the data to JSON
+            //         // convert the data to JSON
 
             const weatherData = JSON.parse(data)
-            // console.log(WeatherData);
-
-            /*  const objectMe = {
-                 name: "Ochwada",
-                 favoriteFood: "Ugali"
-             }
-             console.log(JSON.stringify(objectMe)) */
-
             const temp = Math.floor(weatherData.main.temp - 273.15)
             const descOfData = weatherData.weather[0].description
+            const icon = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png"
 
-/*             res.send("<h1>  The weather is currently- " + descOfData + "<br> and Its now " + temp + "° in " + weatherData.name + ".</h1>");
- */         res.write("<h3>  The weather is currently- " + descOfData + "</h3>")
+
+            res.write("<h3>  The weather is currently- " + descOfData + "</h3>")
             res.write("<h1> and Its now " + temp + " degrees in " + weatherData.name + ".</h1 >")
+            res.write("<img src ='" + icon + "'/>")
             res.send();          //console.log(descOfData);
-
-            // console.log("Its now " + temp + "° in " + weatherData.name );
         });
     });
-
-    //res.send("Server is up and running")
 });
+    /* ---------------             ---------------*/
 
-/* ---------------             ---------------*/
+    app.listen(3000, function () {
+        console.log("server is running on port 3000")
+    });
 
+//res.sendFile( __dirname + '/index.html');
 
-app.listen(3000, function () {
-    console.log("server is running on port 3000")
-});
+// https.get(urlLink, (response) => {
+//     console.log(response.statusCode);
+
+//     response.on('data', (data) => {
+//         // convert the data to JSON
+
+//         const weatherData = JSON.parse(data)
+//         // console.log(WeatherData);
+
+//         /*  const objectMe = {
+//              name: "Ochwada",
+//              favoriteFood: "Ugali"
+//          }
+//          console.log(JSON.stringify(objectMe)) */
+
+//         const temp = Math.floor(weatherData.main.temp - 273.15)
+//         const descOfData = weatherData.weather[0].description
+//         const icon = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png"
+// /*
+
+//  */         res.write("<h3>  The weather is currently- " + descOfData + "</h3>")
+//         res.write("<h1> and Its now " + temp + " degrees in " + weatherData.name + ".</h1 >")
+//         res.write("<img src ='" + icon + "'/>")
+//         res.send();          //console.log(descOfData);
+
+//         // console.log("Its now " + temp + "° in " + weatherData.name );
+//     });
+// });
