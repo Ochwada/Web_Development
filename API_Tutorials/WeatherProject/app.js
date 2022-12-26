52ab558a642c49c8d662a6a39bfcf9ab1bad2d5d
Ochwada <Ochwada@users.noreply.github.com>
2022-12-26 14:37:00 +0100

    res.write

--- API_Tutorials/WeatherProject/app.js
+++ API_Tutorials/WeatherProject/app.js
@@ -7,9 +7,14 @@ const app = express();
 
 /* --------    Web pages     ---------------*/
 
+
+
 app.get('/', (req, res) => {
+
+    //res.sendFile( __dirname + '/index.html');
+
     var urlLink = "https://api.openweathermap.org/data/2.5/weather?q=Spandau,%20Berlin&appid=fd88392e09391d414afd29129da77337&unit=metric"
-    
+
     https.get(urlLink, (response) => {
         console.log(response.statusCode);
 
@@ -19,19 +24,25 @@ app.get('/', (req, res) => {
             const weatherData = JSON.parse(data)
             // console.log(WeatherData);
 
-           /*  const objectMe = {
-                name: "Ochwada",
-                favoriteFood: "Ugali"
-            }
-            console.log(JSON.stringify(objectMe)) */
+            /*  const objectMe = {
+                 name: "Ochwada",
+                 favoriteFood: "Ugali"
+             }
+             console.log(JSON.stringify(objectMe)) */
 
-            const temp = (weatherData.main.temp - 273.15)
+            const temp = Math.floor(weatherData.main.temp - 273.15)
+            const descOfData = weatherData.weather[0].description
 
-            console.log("Its now " + temp + "° in " + weatherData.name );
+/*             res.send("<h1>  The weather is currently- " + descOfData + "<br> and Its now " + temp + "° in " + weatherData.name + ".</h1>");
+ */         res.write("<h3>  The weather is currently- " + descOfData + "</h3>")
+            res.write("<h1> and Its now " + temp + " degrees in " + weatherData.name + ".</h1 >")
+            res.send();          //console.log(descOfData);
+
+            // console.log("Its now " + temp + "° in " + weatherData.name );
         });
     });
 
-    res.send("Server is up and running")
+    //res.send("Server is up and running")
 });
 
 /* ---------------             ---------------*/
