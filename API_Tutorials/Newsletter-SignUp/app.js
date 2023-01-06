f5e1599e1c3b9e14d1596f8cadee5605410950ce
Ochwada <Ochwada@users.noreply.github.com>
2023-01-06 10:54:40 +0100

    fixing css and images

--- API_Tutorials/Newsletter-SignUp/app.js
+++ API_Tutorials/Newsletter-SignUp/app.js
@@ -7,7 +7,7 @@ const https = require("node:https");
 
 const app = express();
 
-app.use(express.static("public")); // read the public folder the css and images are
+app.use(express.static("/public/")); // read the public folder the css and images are, *** added the line for vercel
 
 app.use(bodyParser.urlencoded({
     extended: true
