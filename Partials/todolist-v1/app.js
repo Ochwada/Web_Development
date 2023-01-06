f711ff53b07402f3187250f4998aeb5a82e18c4f
Ochwada <Ochwada@users.noreply.github.com>
2023-01-06 14:44:14 +0100

    1st template (ejs)

--- Partials/todolist-v1/app.js
+++ Partials/todolist-v1/app.js
@@ -17,17 +17,52 @@ app.get('/', (req, res) => {
     
     //res.sendFile( __dirname + '/index.html');
     var today = new Date();
-    var currentDate = today.getTime();
+    var currentTime = today.getTime();
+    var currentDay = today.getDay();
     var day = "";
 
-    if (currentDate === 6 || currentDate === 0){
-        day = "weekend";
+   /*  if (currentDate === 6 || currentDate === 0){
+        //day = "weekend";
+        day = currentDay + ". A Weekend"
         //res.write("its weekend");
     }else{
-        day = "weekday";
+        day = currentDay + " Day. A Weekday"
+        
         //res.sendFile(__dirname + '/index.html');
+    } */
+
+
+    switch(currentDay){
+        case 0:
+            day ="Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day= "Saturday";
+            break;
+        default:
+            console.log("Error: current day is equal to:" + currentDay);
+    
     }
-    res.render("list", { kindOfDay: day }) // "list" is an ejs file from view directory
+
+    res.render("list",{
+        kindOfDay: day
+    })
+    //res.render("list", { kindOfDay: day }) // "list" is an ejs file from view directory
         
 });
 
