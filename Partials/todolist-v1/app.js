09f03112b80938e02eb569df520f856163f189c0
Ochwada <Ochwada@users.noreply.github.com>
2023-01-10 14:52:49 +0100

    todoList and form

--- Partials/todolist-v1/app.js
+++ Partials/todolist-v1/app.js
@@ -8,65 +8,89 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+// --- Variables --- 
+
+var item = ""
 // --------------------
 
 
 
 
 app.get('/', (req, res) => {
-    
+
     //res.sendFile( __dirname + '/index.html');
     var today = new Date();
-    var currentTime = today.getTime();
-    var currentDay = today.getDay();
-    var day = "";
+    /* var currentTime = today.getTime();
+    var currentDay = today.getDay(); */
+    //var day = "";
 
-   /*  if (currentDate === 6 || currentDate === 0){
-        //day = "weekend";
-        day = currentDay + ". A Weekend"
-        //res.write("its weekend");
-    }else{
-        day = currentDay + " Day. A Weekday"
-        
-        //res.sendFile(__dirname + '/index.html');
-    } */
-
-
-    switch(currentDay){
-        case 0:
-            day ="Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day= "Saturday";
-            break;
-        default:
-            console.log("Error: current day is equal to:" + currentDay);
-    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
     }
+    var day = today.toLocaleDateString("en-US", options);
 
-    res.render("list",{
-        kindOfDay: day
-    })
-    //res.render("list", { kindOfDay: day }) // "list" is an ejs file from view directory
+    /*  if (currentDate === 6 || currentDate === 0){
+         //day = "weekend";
+         day = currentDay + ". A Weekend"
+         //res.write("its weekend");
+     }else{
+         day = currentDay + " Day. A Weekday"
+         
+         //res.sendFile(__dirname + '/index.html');
+     } */
+
+    /* 
+        switch(currentDay){
+            case 0:
+                day ="Sunday";
+                break;
+            case 1:
+                day = "Monday";
+                break;
+            case 2:
+                day = "Tuesday";
+                break;
+            case 3:
+                day = "Wednesday";
+                break;
+            case 4:
+                day = "Thursday";
+                break;
+            case 5:
+                day = "Friday";
+                break;
+            case 6:
+                day= "Saturday";
+                break;
+            default:
+                console.log("Error: current day is equal to:" + currentDay);
         
+        } */
+
+    res.render("list", {
+        kindOfDay: day,
+        newListItem: item
+    });
+    //res.render("list", { kindOfDay: day }) // "list" is an ejs file from view directory
+
 });
 
 
+// --- Post request to add a TO DO ----------->
+app.post("/", (req, res) => {
+    item = req.body.newItem
+    //console.log(item)
+    res.redirect("/")
+
+});
+
+// ------------------------------------------->
 
 
 
